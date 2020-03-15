81b812044bb0bab304d6b074fe51b70b7435a0b2
Jeff <lijeff20001@gmail.com>
2020-03-15 22:52:57 +0000

    Fixed some problems of the docker_sql file according to the comments of the code review

--- linux_sql/psql_docker.sh
+++ linux_sql/psql_docker.sh
@@ -1,30 +1,55 @@
 #! /bin/bash
-if [ $# != 2 ];
+
+#make sure the correct number of arguments are given
+if [ $# -lt 1 ] || [ $# -gt 2 ];
 then
-	echo $#
 	echo "Wrong number of arugments"
 	exit 1
 fi
-if [ $1 == "start" ]; then 
+
+# Check that the command is to start or stop docker
+if [ $1 != "start" ] || [ $1 != "stop" ]
+then
+	echo "Incorrect command, use ./psql_docker.sh start | stop [password]"
+	exit 1
+fi
+# Handle starting docker logic
+if [ $1 == "start" ];
+then
+	# If the password is not provided set the password to the default
+	if [ $# -eq 1 ];
+	then
+		export PGPASSWORD = 'password'
+	fi
+	# start docker or give the status if docker is already started
 	systemctl status docker  || systemctl start docker
+
+	# start the container for the first time if needed
 	linecount=`docker ps -af name=jrvs-psql | wc -l`
-	if [ "$linecount" -gt 2 ];
+	if [ "$linecount" -lt 2 ];
 	then
 		docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
 	fi
+
 	docker container start jrvs-psql
+
+	#check if the volume exists
 	docker volume inspect pgdata
 	if [ "$?" -ne  0 ];
 	then
 		docker volume create pgdata
 	fi
+	# start the psql interface
 	psql -h localhost -U postgres -W
+
 	exit 0
 fi
+
 if [ $1 == "stop" ];
-then	
+then
+	# stop and exit from docker
 	docker container stop jrvs-psql
 	systemctl stop docker
-	exit 1
+	exit 0
 fi
 exit 1

148982b25616cdf42407f43c75c6ea116c758cd8
Jeff <jeffl.li@mail.utoronto.ca>
2020-04-27 12:13:00 -0400

    JDBC

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/OrderLine.java
@@ -0,0 +1,72 @@
+package ca.jrvs.apps.jdbc;
+
+public class OrderLine {
+    private int quantity;
+    private String productCode;
+    private String productName;
+    private int productSize;
+    private String productVariety;
+    private double productPrice;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductSize() {
+        return productSize;
+    }
+
+    public void setProductSize(int productSize) {
+        this.productSize = productSize;
+    }
+
+    public String getProductVariety() {
+        return productVariety;
+    }
+
+    public void setProductVariety(String productVariety) {
+        this.productVariety = productVariety;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+
+    @Override
+    public String toString() {
+        // I don't think formatting JSON done here (if anywhere)
+        return "{" +
+                "quantity=" + getQuantity() +
+                ", productCode='" + getProductCode()  +
+                ", productName='" + getProductName() +
+                ", productSize=" + getProductSize() +
+                ", productVariety='" + getProductVariety() +
+                ", productPrice=" + getProductPrice() +
+                '}';
+    }
+}
